6985276db4091107cbf03db0c582f82e1ca2a3b4
anuittt97 <45379792+antt97@users.noreply.github.com>
2018-12-21 01:24:13 +0700

    Socket join room

---
+++ Hubs/GameHub.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System.Threading.Tasks;
+using MvcApp.Models;
+
+namespace BattleShip {
+
+  [HubName("GameHub")]
+  public class GameHub : Hub {
+    public void JoinRoom(string id) { //Socket join Room
+      Groups.Add(Context.ConnectionId, id);
+    }
+
+    public override Task OnConnected() {
+      return base.OnConnected();
+    }
+
+    public override Task OnDisconnected(bool stopCalled) {
+      return base.OnDisconnected(stopCalled);
+    }
+  }
+}
