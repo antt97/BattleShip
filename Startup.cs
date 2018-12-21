dd41ef446bb675b83c7d52e0058352570f6fc7f4
anuittt97 <45379792+antt97@users.noreply.github.com>
2018-12-21 08:46:58 +0700

    1

---
+++ Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BattleShip.Startup))]
+namespace BattleShip {
+  public partial class Startup {
+    public void Configuration(IAppBuilder app) {
+      ConfigureAuth(app);
+      app.MapSignalR();
+
+    }
+  }
+}
